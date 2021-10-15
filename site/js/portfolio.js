c57eb17b30731fe745ee351f0a30e09a938a9c07
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-15 11:47:08 +0200

    Menu links color can now be changed

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,6 +8,7 @@ const nameText = document.getElementById('name')
 const nameAndColorButton = document.getElementById('name-and-color-button')
 const backGroundColor = document.getElementsByClassName('pink-bg')
 const textColor = document.getElementsByClassName('pink-text')
+const menuLinks = document.getElementsByClassName('menu-link')
 
 nameAndColorButton.addEventListener("click", function() {
     let color = prompt("What color would you like ?")
@@ -17,6 +18,9 @@ nameAndColorButton.addEventListener("click", function() {
     }
     for (let i = 0 ; i < textColor.length ; i++) {
         textColor[i].style.color = color
+    }
+    for (let i = 0 ; i < menuLinks.length ; i++) {
+        menuLinks[i].style.color = color
     }
     nameText.innerHTML = newName
     nameText.style.color = "white"
