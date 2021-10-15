28978ff1523fa0580ca2c42a1f0d0e0053b548d9
Benjamin <b.girardpro@gmail.com>
2021-10-15 10:24:35 +0200

    Add a click event on change name and color button

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,14 @@
+const userName = prompt("Enter your name: ")
 const avatar = document.getElementById("avatar");
+const btnChangeName = document.getElementById("btnChangeName")
+const name = document.getElementById("name")
+
 
 avatar.addEventListener("click", () => {
     avatar.src = "image/avatar-bis.svg";
 })
+
+btnChangeName.addEventListener("click", () => {
+    name.innerHTML = userName
+    name.style.color = "white"
+} )
