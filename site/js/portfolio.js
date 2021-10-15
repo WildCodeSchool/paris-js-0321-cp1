a5cc0841a27af20eab5e9d8e6241c530a8330913
Benjamin <b.girardpro@gmail.com>
2021-10-15 10:33:58 +0200

    Add a prompt for new background color and changed presentation section and btn change name bgcolor

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,7 +1,7 @@
-const userName = prompt("Enter your name: ")
 const avatar = document.getElementById("avatar");
 const btnChangeName = document.getElementById("btnChangeName")
 const name = document.getElementById("name")
+const description = document.getElementsByClassName("description")[0]
 
 
 avatar.addEventListener("click", () => {
@@ -9,6 +9,11 @@ avatar.addEventListener("click", () => {
 })
 
 btnChangeName.addEventListener("click", () => {
+    const newBgColor = prompt("Enter the new color")
+    const userName = prompt("Enter your name: ")
     name.innerHTML = userName
     name.style.color = "white"
+    description.classList.remove("pink-bg")
+    description.style.backgroundColor = newBgColor
+    btnChangeName.style.backgroundColor = newBgColor
 } )
