0f53b1a9f848aad72fed022c06d0aaddc7f28eb1
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-10-15 11:13:18 +0200

    Completed 2.1

---
+++ site/js/portfolio.js
@@ -0,0 +1,9 @@
+const newAvatarImage = document.getElementById("avatar")
+
+newAvatarImage.addEventListener('click', function(){
+
+    newAvatarImage.src = "./image/avatar-bis.svg";
+  
+  
+  
+  })
