144bcce11999720ba6566d63cf340a3aada330f9
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-15 11:25:01 +0200

    You can modify the name via a prompt activated by a button (the name becomes white)

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,12 @@ const avatarPicture = document.getElementById('avatar-picture')
 avatarPicture.addEventListener("click", function() {
     avatarPicture.src = './image/avatar-bis.svg'
 })
+
+const nameText = document.getElementById('name')
+const nameButton = document.getElementById('name-button')
+
+nameButton.addEventListener("click", function() {
+    let newName = prompt("What's your name ?")
+    nameText.innerHTML = newName
+    nameText.style.color = "white"
+})
