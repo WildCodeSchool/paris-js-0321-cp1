c431ffd0c2cfcb20bdb3462772f0343c51f857e6
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-10-15 13:05:38 +0200

    Uncompleted 2.3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -11,6 +11,15 @@ newAvatarImage.addEventListener('click', function(){
   const button = document.getElementById("buttonName")
   button.addEventListener('click', function() {
     let answer = window.prompt("Enter your name");
+    let answerColor = window.prompt("Enter your color");
     document.getElementById("name").innerHTML = answer 
     document.getElementById("name").style.color = 'white'
+    document.getElementsByClassName("pink-bg").style.backgroundColor = answerColor;
+    
+
 })
+
+    
+    
+
+
