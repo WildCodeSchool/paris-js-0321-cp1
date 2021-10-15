b2674f975c2b83a7a5c1f93335229b7bf5f2d0f9
110111-1 <moty.philippe@free.fr>
2021-10-15 12:22:41 +0200

    Seconde Part finish with some Js part imcompleted

---
+++ site/js/portfolio.js
@@ -0,0 +1,20 @@
+let myImage = document.querySelector(".banner-img img");
+myImage.addEventListener("click", function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "./image/avatar.png") {
+    myImage.setAttribute("src", "./image/avatar-bis.svg");
+  } else {
+    myImage.setAttribute("src", "./image/avatar.png");
+  }
+});
+
+let myNameName = document.querySelector("#name");
+let selectName = document.querySelector(".descrition-btn");
+selectName.addEventListener("click", function () {
+  let myColor = prompt("Enter Color");
+  let myName = prompt("Enter your name ");
+  myNameName.textContent = `${myName}`;
+
+  let myOtherColor = document.querySelector(".description pink-bg");
+   myOtherColor.style.backgroundColor = '${myOtherColor}';
+});
