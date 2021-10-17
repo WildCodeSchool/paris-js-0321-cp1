ab018bb05045c8d2b851d32ff357c6c125af25c4
Your Name <you@example.com>
2021-10-17 17:10:45 +0200

    Completed step 3 & 4

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,7 +8,20 @@ const btn = document.querySelector('#btn');
 btn.innerHTML = "Modify text and color";
 btn.addEventListener('click', function(){
    const answer = prompt("Enter your name");
+   const allColor = prompt("Enter your color");
    document.getElementById('name').innerText = answer;
    document.getElementById('name').style.color= "white";
-   
+
+   /*const answerColor = prompt("Enter your color");
+   document.querySelector('.pink-bg').style.backgroundColor = answerColor;  */
+
+   const bg = document.querySelectorAll('.pink-bg');
+   bg.forEach((event) => {
+       event.style.backgroundColor = allColor;
+
+   })
+   const text = document.querySelectorAll('.pink-text');
+   text.forEach((e) => {
+       e.style.color = allColor;
+   });
 })
