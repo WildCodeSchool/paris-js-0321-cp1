c68d5873cb23f92f2f991f1460cea09065923bf5
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2021-10-18 23:21:05 +0200

    Js exercise done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,23 +1,32 @@
 // We set the source of the image avatar to a new url
-const avatarChanged = document.getElementsByClassName('.avatar');
-    avatarChanged.addEventListener("click", function() {
-	avatarChanged.src = "image/avatar-bis.svg";
-    });
+const imgAvatar = document.querySelector('.avatar');
+    imgAvatar.addEventListener('click', function() {
 
-const nameChanged = document.querySelector('.nameChanged');
+    imgAvatar.src = "image/avatar-bis.svg";
+    })
+
+const nameChanged = document.querySelector('#btm');
+nameChanged.innerHTML="Modify text and color";
 nameChanged.addEventListener("click", function() {
     const name=prompt("What's your name?");
+    
+    document.getElementById('name').innerHTML=name;
+    document.getElementById('name').style.color="white";
+    const btmColor=prompt("What's color do you want?");
+    const anotherColor=document.querySelectorAll(".pink-bg");
+    anotherColor.forEach((event)=>{
+        event.style.backgroundColor=btmColor;
+    })
+    /*
+    for (let i=0; i<anotherColor.length;i++){
+        anotherColor[i].style.backgroundColor*/
+});
+
+/*
+anotherColor.addEventListener("click", function() {
+    
     const newName=document.querySelector("#name")
     newName.innerHTML = `${name}`;
     newName.style.color= "white";
-};
-const nameColor= document.querySelector('.nameColor');
-nameColor.addEventListener("click", function() {
-    const colorChanged=prompt("What color do you want?");
-    const newColor=document.querySelector('.description pink-bg')
-    newColor.style.background-color= `${colorChanged}`;
-};
-const anotherColor=document.getElementsByClassName(".pink-bg")
-for (let i=0; i<anotherColor.length;i++){
-anotherColor[i].style.backgroundColor = "#750ff7"
-}
+
+}*/
